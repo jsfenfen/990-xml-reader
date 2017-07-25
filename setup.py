e2478cf21e686a58b3580a9f756f66cc8144a127
Jacob Fenton <jsfenfen@gmail.com>
2017-07-25 09:34:42 -0700

    add _version, move tests around

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
+import os
 
-setup(name="irs_reader",
-      version="0.1",
+
+NAME = "irs_reader"
+HERE = os.path.abspath(os.path.dirname(__file__))
+version_ns = {}
+with open(os.path.join(HERE, NAME, '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+
+setup(name=NAME,
+      description="Turn the IRS' versioned XML 990's into python objects with original line number and description.",
+      version=version_ns['__version__'],
       setup_requires=["setuptools",],
       install_requires=["requests", "xmltodict"],
       tests_require=["nose",],
       packages=["irs_reader"],
-
+      package_dir={'irs_reader': 'irs_reader'},
+      package_data={'irs_reader': ['data/*/*.json']},
       entry_points={
           "console_scripts": ["readxml=irs_reader.cli:main"]
       },
