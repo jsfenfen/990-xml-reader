fa6f85e9f10f6df57f2c1860f0e810247917d41a
Jacob Fenton <jsfenfen@gmail.com>
2017-09-12 16:42:34 -0700

    cleanup

--- irs_reader/flatten_utils.py
+++ irs_reader/flatten_utils.py
@@ -1,13 +1,13 @@
 import collections
 #  Mostly from: http://stackoverflow.com/a/6027615
 
+
 def flatten(d, parent_key='', sep='/'):
     items = []
-    #print "flatten '%s'" % (d)
+    # print "flatten '%s'" % (d)
     if d:
         for k, v in d.items():
             new_key = parent_key + sep + k if parent_key else k
-            #print new_key
             if isinstance(v, collections.MutableMapping):
                 items.extend(flatten(v, new_key, sep=sep).items())
             else:
