ac90459a83595b015642ef131b113248e6550ce6
Jacob Fenton <jsfenfen@gmail.com>
2017-09-18 09:40:54 -0700

    local settings example

--- irs_reader/local_settings-example.py
+++ irs_reader/local_settings-example.py
@@ -1,3 +1,16 @@
+import os
+from .dir_utils import mkdir_p
 
 
-IRS_READER_ROOT = "/Local/path/to/files/"
+IRS_READER_ROOT = "/path/to/irsreader/990-xml-reader"
+
+# This is the URL to amazon's bucket, could use another synced to it
+IRS_XML_HTTP_BASE = "https://s3.amazonaws.com/irs-form-990"
+
+# The directory we put files in while we're processing them
+WORKING_DIRECTORY = (os.path.join(IRS_READER_ROOT, "XML") )
+
+# Helpful to keep these around for lookup purposes
+INDEX_DIRECTORY = (os.path.join(IRS_READER_ROOT, "CSV") )
+
+mkdir_p([WORKING_DIRECTORY, INDEX_DIRECTORY])
