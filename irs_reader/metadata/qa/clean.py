975c25a20418e6b7c248a46f6c3e98a934feacc3
Jacob Fenton <jsfenfen@gmail.com>
2017-10-25 12:54:09 -0700

    comments

--- irs_reader/metadata/qa/clean.py
+++ irs_reader/metadata/qa/clean.py
@@ -1,9 +1,8 @@
 """
-Reads the concordance file and writes it back out in the project's "standard" csv format
-https://github.com/Nonprofit-Open-Data-Collective/irs-efile-master-concordance-file/tree/master/qa
-Also removes "NA". Historically these had been included, though should be gone by now. 
+Reads the .csv files and writes them back out in the project's "standard" csv format
+Also removes "NA". 
 
-Writes to efiler_master_concordance_reformatted.csv -- check the output is good before overwriting
+Writes to FILENAME_standardformat.csv -- check the output is good before overwriting
 """
 
 from .utils import get_group_writer, get_variable_writer, get_schedule_parts_writer, fix_row
